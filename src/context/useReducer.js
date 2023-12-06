0a36318096bec86113ce7b99a2df2916862e94ec
aarya moktan <aaryamoktan2055@gmail.com>
2023-12-06 00:23:04 -0800

    commit first

--- src/context/useReducer.js
+++ src/context/useReducer.js
@@ -1,12 +1,23 @@
-import { useReducer } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import { reducer } from "./reducer";
 
 export const Nameee = ()=>
 {
+    const [set,sets] = useState(0);
+    const name1 = useRef(0)
+    console.log(name1.current)
+    useEffect(()=>
+    {
+        name1.current = name1.current+1;
+    })
+    
     const [name,dispatch] = useReducer(reducer,0)
     return(
-        <>
+        <><input type="text" value={set} onChange={(e)=>sets(e.target.value)}/>{name1.current}
+        <br/>
         {name}
+        
+        
             <button type="submit" onClick={()=>dispatch({type:"INC"})}>increase</button>
         </>
     )
